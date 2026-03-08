19f7195d6d9219f9d106c6edfc6f217ce07440c7
kevinhilary <khilary@byupathway.edu>
2026-03-08 22:02:19 +0300

    finalwork

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -5,3 +5,10 @@ navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
     navlinks.classList.toggle('show');
  });
+
+ const year = document.getElementById('year');
+ year.textContent = new Date().getFullYear();
+
+ const lastModification = document.getElementById('lastmodification');
+ const lastModDate = new Date(document.lastModified);
+ lastModification.textContent = `Last Modification: ${lastModDate.toLocaleString()}`;
